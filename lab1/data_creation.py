29dc6ec4de743aa5d7cc21ae7e77c4e735501750
Sergei Vasilev <sega.vasiev@gmail.com>
2024-04-10 02:00:40 +1100

    rebuild

--- lab1/data_creation.py
+++ lab1/data_creation.py
@@ -1,34 +1,85 @@
-"""Данный скрипт создает различные наборы данных, описывающие некий процесс (например, изменение уровня содержания CO2 в атмосферном воздухе).
-Таких наборов должно быть несколько, в некоторые данные можно включить аномалии или шумы.
-Часть наборов данных должна быть сохранена в папке «train», другая часть — в папке «test».
-Данные сохраняются в формате .npy, где первый столбец — часы от момента начала измерений, второй — уровень содержания CO2 в условных единицах."""
-
-import numpy as np
+"""Данный скрипт выкачивает данные реальных погодных наблюдений.
+В качестве параметров принимает:
+- дату начала и конца периода наблюдений,
+- географические координаты места наблюдений.
+"""
 import os
 
+import pandas as pd
+import argparse
 
-def create_data(path: str, file_name: str):
-    """Создает набор данных, описывающий изменение уровня содержания CO2 в атмосферном воздухе.
-    Включает аномалии и шумы.
-    Сохраняет в папку в формате npz."""
-    t = np.linspace(0, 100, 1000)
-    y = 300 + 10 * np.sin(t) + 5 * np.random.randn(1000)
-    y[50:100] = y[50:100] + 10
-    y[200:210] = y[200:210] + 20
-    y[300:310] = y[300:310] + 30
-    y[400:410] = y[400:410] + 40
-    y[500:510] = y[500:510] + 50
-    y[600:610] = y[600:610] + 60
-    y[700:710] = y[700:710] + 70
-    y[800:810] = y[800:810] + 80
-    y[900:910] = y[900:910] + 90
-    data = np.vstack([t, y]).T
-    np.save(os.path.join(path, file_name), data)
+from meteostat import Point, Hourly
+from datetime import datetime
+from typing import Optional, Tuple
+
+from sklearn.model_selection import train_test_split
+
+
+def get_weather_data(lat: float, lon: float, alt: Optional[float], start_date: datetime,
+                     end_date: datetime) -> pd.DataFrame:
+    """
+    Get weather data for the specified location in the specified period
+    :param lat: Latitude of the location
+    :param lon: Longitude of the location
+    :param alt: Altitude of the location (optional)
+    :param start_date: Beginning of the period
+    :param end_date: End of the period
+    :return: DataFrame with weather data
+    """
+    # Create Point
+    location = Point(lat, lon, alt)
+    # Get hourly data
+    hourly = Hourly(location, start_date, end_date)
+    hourly = pd.DataFrame(hourly.fetch())
+    # Delete columns with missing values
+    hourly = hourly.dropna(axis=1, how='any')
+    return hourly
+
+
+# Функция разделения данных на обучающую и тестовую выборки
+def split_data(data: pd.DataFrame, test_size: float = 0.3) -> Tuple[pd.DataFrame, pd.DataFrame]:
+    """
+    Split data into training and test sets
+    :param data: DataFrame with data
+    :param test_size: The proportion of the dataset to include in the test split
+    :return: Training and test sets
+    """
+    # Split data
+    train, test = train_test_split(data, test_size=test_size, shuffle=False)
+    return train, test
 
 
 if __name__ == '__main__':
+    # Parse arguments
+    parser = argparse.ArgumentParser(description='Download weather data')
+    parser.add_argument('--lat', type=float, required=True, help='Latitude of the location (ie. 7.8804)')
+    parser.add_argument('--lon', type=float, required=True, help='Longitude of the location (ie. 98.3923')
+    parser.add_argument('--alt', type=float, help='Altitude of the location (ie. 10.0)')
+    parser.add_argument('--start_date', type=str, required=True, help='Beginning of the period (ie. 2020-01-01 00:00:00)')
+    parser.add_argument('--end_date', type=str, required=True, help='End of the period (ie. 2020-12-31 23:00:00)')
+    # Simulate command line arguments
+    #argv = ['--lat', '7.8804', '--lon', '98.3923', '--alt', '10.0', '--start_date', '2020-01-01 00:00:00', '--end_date', '2020-1-31 23:00:00']
+    #args = parser.parse_args(argv)
+
+    args = parser.parse_args()
+
+    # Convert dates to datetime
+    start_date = datetime.strptime(args.start_date, '%Y-%m-%d %H:%M:%S')
+    end_date = datetime.strptime(args.end_date, '%Y-%m-%d %H:%M:%S')
+
+    # Get weather data
+    data = get_weather_data(args.lat, args.lon, args.alt, start_date, end_date)
+
+    # Split data
+    train, test = split_data(data)
+
+    # Save data
     paths = ['train', 'test']
-    file_names = ['train_data', 'test_data']
-    for path, file_name in zip(paths, file_names):
+    for path in paths:
         os.makedirs(path, exist_ok=True)
-        create_data(path, file_name)
+    train_path = 'train/train_data.csv'
+    test_path = 'test/test_data.csv'
+    train.to_csv(train_path, index=False)
+    test.to_csv(test_path, index=False)
+    # Stdout message with paths
+    print(f'{train_path}, {test_path}')
