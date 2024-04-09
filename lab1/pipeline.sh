5294aacefecff91e588434c87d6ade781045c2c3
Sergei Vasilev <sega.vasiev@gmail.com>
2024-04-10 02:09:05 +1100

    rebuild

--- lab1/pipeline.sh
+++ lab1/pipeline.sh
@@ -15,7 +15,8 @@ source .venv/bin/activate
 pip install -r requirements.txt
 
 # Запуск всех скриптов
-python3 data_creation.py '--lat', '7.8804', '--lon', '98.3923', '--alt', '10.0', '--start_date', '2020-01-01 00:00:00', '--end_date', '2020-1-31 23:00:00'
+output_paths=$(python3 data_creation.py --lat 7.8804 --lon 98.3923 --alt 10.0 --start_date "2020-01-01 00:00:00" --end_date "2020-1-31 23:00:00")
+echo $output_paths
 #python3 data_preprocessing.py
 #python3 model_preparation.py
 #python3 model_testing.py
